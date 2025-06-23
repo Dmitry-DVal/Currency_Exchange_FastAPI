ebf7efe8f70fbbf28359fffcb1718e4a6b671e84
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2025-06-23 19:05:25 +0400

    Added pydantic schemes for currencies

--- src/сurrency_exchange_app/api/currency.py
+++ src/сurrency_exchange_app/api/currency.py
@@ -1,44 +1,65 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
+from сurrency_exchange_app.schemas.currency import CurrencyResponse, CurrencyCreate
 
 router = APIRouter(tags=["Валюты"])
 
 currencies = [
-    {
-        "id": 1,
-        "name": "United States dollar",
-        "code": "USD",
-        "sign": "$"
-    },
-    {
-        "id": 2,
-        "name": "Euro",
-        "code": "EUR",
-        "sign": "€"
-    }
+    CurrencyResponse(
+        id=1,
+        name="United States dollar",
+        code="USD",
+        sign="$"
+    ),
+    CurrencyResponse(
+        id=2,
+        name="Euro",
+        code="EUR",
+        sign="€"
+    )
 ]
 
 
-@router.get("/currency/{code}")
-async def get_currency(code: str) -> dict: # Должна возвращать модель валюты
-    return {
-    "id": 0,
-    "name": "Euro",
-    "code": code,
-    "sign": "€"
-}
+@router.get(
+    "/currency/{code}",
+    response_model=CurrencyResponse,
+    summary="Получить валюту по коду",
+    description="Возвращает полную информацию о валюте по её трёхбуквенному коду"
+)
+async def get_currency(
+        code: str) -> CurrencyResponse:  # Должна возвращать модель валюты
+    currency = next((c for c in currencies if c.code == code.upper()), None)
+    if not currency:
+        raise HTTPException(status_code=404, detail="Currency not found")
+    return currency
 
-@router.get("/currencies")
-async def get_currencies() -> list[dict]: # Возвращает список моделей
+
+@router.get("/currencies",
+            summary="Получить все доступные валюты",
+            description="Возвращает полную информацию о всех валютых"
+            )
+async def get_currencies() -> list[CurrencyResponse]:  # Возвращает список моделей
     return currencies
 
-@router.post("/currencies")
-async def post_currencies(name, code, sign) -> dict:
-    currencies.append(
-        {
-            "id": len(currencies) + 1,
-            "name": name,
-            "code": code,
-            "sign": sign
-        }
+
+@router.post(
+    "/currencies",
+    response_model=CurrencyResponse,
+    summary="Добавить валюту",
+    description="Создаёт новую валюту в системе",
+    response_description="Данные созданной валюты",
+    responses={
+        400: {"description": "Валюта с таким кодом уже существует"},
+        201: {"description": "Валюта успешно создана"}
+    }
+)
+async def create_currency(currency_data: CurrencyCreate) -> CurrencyResponse:
+    if any(c.code == currency_data.code for c in currencies):
+        raise HTTPException(status_code=400, detail="Currency already exists")
+
+    new_currency = CurrencyResponse(
+        id=len(currencies) + 1,
+        **currency_data.model_dump()
     )
-    return {"success": True}
+    currencies.append(new_currency)
+    return new_currency
---
+++ src/сurrency_exchange_app/schemas/currency.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, field_validator
+
+
+class CurrencyBase(BaseModel):
+    name: str = Field(min_length=2, max_length=50, examples=["United States dollar"],
+                      description="Полное название валюты")
+    code: str = Field(min_length=3, max_length=3, pattern=r"^[A-Za-z]+$",
+                      examples=["USD"])
+    sign: str = Field(min_length=1, max_length=5, examples=["$"])
+
+    @field_validator("code")
+    def code_to_upper(cls, v):
+        return v.upper()
+
+
+class CurrencyCreate(CurrencyBase):
+    pass
+
+
+class CurrencyResponse(CurrencyBase):
+    id: int
