b0d77904f9be15a01ac25806c3b33995c63d866b
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2025-06-30 17:56:28 +0400

    Added alembic

--- src/currency_exchange_app/db/base.py
+++ src/currency_exchange_app/db/base.py
@@ -3,5 +3,12 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     """Базовый класс для всех ORM моделей"""
-    pass
+
+    def __repr__(self):
+        cols = []
+        for col in self.__table__.columns.key():
+            cols.append(f"{col}={getattr(self, col)}")
+        # for col in self.__table__.columns:
+        #     cols.append(f"{col.name}={getattr(self, col.name)}")
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
 
